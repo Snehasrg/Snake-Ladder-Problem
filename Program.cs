6f419726d824f9d6176e7837386647c57ae96968
Snehasrg <sghongade642@gmail.com>
2023-06-14 14:30:59 +0530

    [sneha]Added program for snake and ladder

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+            SnakeLadder.GameStart();
+        }
+    }
+}
---
+++ SnakeLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class SnakeLadder
+    {
+        public static void GameStart()
+        {
+            int position = 0;
+            int player1;
+
+            player1 = position;
+            Console.WriteLine("Player 1 position is " + player1);
+        }
+    }
+}
